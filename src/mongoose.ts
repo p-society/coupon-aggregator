8439d613461193a5b99eebe14ad2b49b0e271c2a
neoandmatrix <tusharannand@gmail.com>
2024-09-21 14:23:43 +0530

    add docker integration for local testing

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -3,7 +3,9 @@ import { Application } from "./declarations";
 import logger from "./logger";
 
 export default function (app: Application): void {
-  mongoose.connect(app.get("mongodb")).catch((err) => {
+  mongoose.connect(app.get("mongodb")).then(()=>{
+    console.log("connection successful")
+  }).catch((err) => {
     logger.error(err);
     process.exit(1);
   });
